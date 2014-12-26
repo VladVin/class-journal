683471de0ca297f3078d908eb0eac19e07c7a98d
spacedmi <spacedmi@gmail.com>
2014-12-27 01:39:33 +0300

    Added Add and Delete functions

--- journal.ru/www/query.php
+++ journal.ru/www/query.php
@@ -1,22 +1,18 @@
 ﻿<?php 
-	// Script for processing queries from index.php
-	include("functions.php");
-	prepareDB();
+    // Script for processing queries from index.php
+    include("functions.php");
+    prepareDB();
 
-	switch ($_POST['action'])
-	{
-		case "add":
-			if (($_POST['student'] == "") || ($_POST['subject'] == "") || ($_POST['mark'] == "") || ($_POST['date'] == ""))
-			{
-				die("Для добавления необходимо заполнить все поля!");
-			}
-			else echo "Оценка успешно добавлена";
-		break;
-		case "show":
-			echo "SHOW OK";
-		break;
-		default:
-			die("SOMETHING WRONG");
-		break;
-	}
+    switch ($_POST['action'])
+    {
+        case "add":
+            addRecord($_POST);
+        break;
+        case "show":
+            echo "SHOW OK";
+        break;
+        default:
+            die("SOMETHING WRONG");
+        break;
+    }
 ?>
